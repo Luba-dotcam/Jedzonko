9942b092f31537560a36682ccdd05f60f1047e50
mactonight95 <gregi95@op.pl>
2023-06-28 22:48:34 +0200

    Add recipes count on /main

--- jedzonko/views.py
+++ jedzonko/views.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 from django.shortcuts import render
 from django.views import View
+from .models import Recipe
 
 
 class IndexView(View):
@@ -14,8 +15,11 @@ class IndexView(View):
 class DashboardView(View):
 
     def get(self, request):
-        return render(request, template_name='dashboard.html')
-
+        if request.method == 'GET':
+            recipes_count = Recipe.objects.count()
+            return render(request, template_name='dashboard.html', context={'recipes_count': recipes_count})
+        else:
+            return render(request, template_name='dashboard.html')
 
 class RecipeView(View):
     def get(self, request):

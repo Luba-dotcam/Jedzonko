7c9273a183a0ca0f1d502ee0f2a043208ee5b0d9
Luba-dotcam <luba82@gmail.com>
2023-06-28 11:13:54 +0200

    Landing page

--- scrumlab/urls.py
+++ scrumlab/urls.py
@@ -13,12 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
-
+from django.contrib import admin
 from jedzonko.views import IndexView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', IndexView.as_view()),
+    path('', IndexView.as_view(), name='index'),
 ]

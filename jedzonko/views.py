4b5210da9f2746446dec4ce3901f0be13a8fa8be
mariuszfortuna <mariusz.fortuna@protonmail.com>
2023-06-28 18:42:01 +0200

    link do /recipe/list

--- jedzonko/views.py
+++ jedzonko/views.py
@@ -8,4 +8,9 @@ class IndexView(View):
 
     def get(self, request):
         ctx = {"actual_date": datetime.now()}
-        return render(request, "test.html", ctx)
+        return render(request, "index.html", ctx)
+
+
+class RecipeView(View):
+    def get(self, request):
+        return render(request, "app-recipes.html")
